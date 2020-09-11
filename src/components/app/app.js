f1c81b8d9f45bec41caa4f15dbf48f0fc56d7575
NikV34 <25172610+NikV34@users.noreply.github.com>
2020-09-12 00:24:27 +0200

    testing context

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import withQuestionnaireService from '../hoc';
 
 import './app.css';
 
-const App = () => {
-  return <div>App</div>
+const App = ({questionnaireService}) => {
+  const list = questionnaireService.getQuestions().map((item) => {
+    return (
+      <li>
+        <ul>
+          <li>{item.id}. {item.question}</li>
+          <li>{item.task}</li>
+          <li>
+            <ul>
+              {item.option_list.map((option) => {
+                return <li>{option}</li>
+              })}
+            </ul>
+          </li>
+          <br/>
+          <li>Answer: {item.answer}</li>
+          <li>{item.explanation}</li>
+        </ul>
+        <br/>
+      </li>
+    )
+  })
+
+  return <ul>{
+    list
+  }</ul>
 }
 
-export default App;
+export default withQuestionnaireService()(App);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/app';
 
-ReactDOM.render(<App />,
+import App from './components/app';
+import ErrorBoundary from './components/error-boundary';
+import { QuestionnaireServiceProvider } from './components/questionnaire-service-context';
+import QuestionnaireService from './services/questionnaire-service';
+
+import './index.css';
+
+const questionnaireService = new QuestionnaireService();
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <QuestionnaireServiceProvider value={questionnaireService}>
+        <App />
+    </QuestionnaireServiceProvider>
+  </ErrorBoundary>,
   document.getElementById('root'));
