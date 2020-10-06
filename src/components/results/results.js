c5419176e8c8ca8c50f123333913ee9d4928795c
NikV34 <25172610+NikV34@users.noreply.github.com>
2020-10-06 12:13:33 +0200

    update Results

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -1,30 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './results.css';
 
-const Results = ({progress, total, passed, failed}) => {
-return (
-  <div className="page-header">
-    <h1 className="page-headline">JavaScript Questionnaire</h1>
-    <div className="results">
-      <h3 className="results-headline">Total Score:</h3>
-      <div className="results-score-list">
+const Results = ({ progress, total, passed, failed }) => {
+
+  const renderItems = (itemList) => {
+    return itemList.map((item) => {
+      return (
         <div className="results-score-item">
-          Answered: <b>{`${progress} / ${total}`}</b>
+          {item}
         </div>
-        <div className="results-score-item">
-          Remaining: <b>{total - progress}</b>
+      )
+    })
+  };
+
+  const valueList = [
+    <span>Answered: <b>{`${ progress } / ${ total }`}</b></span>,
+    <span>Remaining: <b>{ total - progress }</b></span>,
+    <span>Passed: <span className="passed">{ passed }</span></span>,
+    <span>Failed: <span className="failed">{ failed }</span></span>
+  ]
+
+  return (
+    <div className="page-header">
+      <h1 className="page-headline">JavaScript Questionnaire</h1>
+      <div className="results">
+        <h3 className="results-headline">Total Score:</h3>
+        <div className="results-score-list">
+          { renderItems(valueList) }
         </div>
-        <div className="results-score-item">
-          Passed: <span className="passed">{passed}</span>
-        </div>
-        <div className="results-score-item">
-          Failed: <span className="failed">{failed}</span>
-        </div>
-      </div>        
-    </div>
+      </div>
     </div>
   )
+}
+
+Results.propTypes = {
+  progress: PropTypes.number,
+  total: PropTypes.number,
+  passed: PropTypes.number,
+  failed: PropTypes.number
 }
 
 export default Results;
