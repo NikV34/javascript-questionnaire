d5013935abfb5d40d3629932196fe5c78e636c20
NikV34 <25172610+NikV34@users.noreply.github.com>
2020-10-08 00:32:43 +0200

    fix empty task

--- src/components/hoc/with-highlighting.js
+++ src/components/hoc/with-highlighting.js
@@ -4,11 +4,19 @@ import Highlight from 'react-highlight.js';
 
 const withHighlighting = (Wrapped) => {
   return (props) => {
-    return (
-      <Highlight language={"javascript"}>
-        <Wrapped {...props} />
-      </Highlight>
-    )
+    if (props.task) {
+      const regex = /\w+$/gm;
+      const language = props.task.match(regex) ? props.task.match(regex)[0] : 'javascript';
+      const task = props.task.replace(language, '').replace('\n', '');
+      const newProps = { ...props, task: task }
+      return (
+        <Highlight language={ language }>
+          <Wrapped { ...newProps } />
+        </Highlight>
+      )
+    } else {
+      return null
+    }
   }
 };
 
