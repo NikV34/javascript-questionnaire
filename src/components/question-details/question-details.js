3ae9bfc5faa80acab9e31a5a28afa2f62de617db
NikV34 <25172610+NikV34@users.noreply.github.com>
2020-10-10 20:03:37 +0200

    fix button Next position

--- src/components/question-details/question-details.js
+++ src/components/question-details/question-details.js
@@ -7,7 +7,7 @@ import QuestionOptionList from '../question-option-list';
 import './question-details.css';
 
 const QuestionTask = ({ task }) => {
-  return <span>{ task }</span>;
+  return <span>{task}</span>;
 }
 
 const QuestionDetails = ({ question, onToggleQuestionOpened, questionAnswered }) => {
@@ -24,16 +24,16 @@ const QuestionDetails = ({ question, onToggleQuestionOpened, questionAnswered })
 
   return (
     <div className="question-details">
-      <h2 className="question-details-headline">{`${ question.id }. ${ question.question }`}</h2>
+      <h2 className="question-details-headline">{`${question.id}. ${question.question}`}</h2>
       <div className="question-details-task">
-        <HighlightedQuestionTask task={ question.task } />
+        <HighlightedQuestionTask task={question.task} />
       </div>
-      <QuestionOptionList question={ question } onAnsweredQuestion={ onAnsweredQuestion }/>
+      <QuestionOptionList question={question} onAnsweredQuestion={onAnsweredQuestion} />
       <div className="question-details-button-group" role="group" aria-label="Basic example">
-        <button type="button" className="question-details-button" onClick={ onToggleQuestionOpened('prev') } >
+        <button type="button" className="question-details-button" onClick={onToggleQuestionOpened('prev')} >
           <span className="question-details-button-prev">Previous</span>
         </button>
-        <button type="button" className="question-details-button" onClick={ onToggleQuestionOpened('next') } >
+        <button type="button" className="question-details-button" onClick={onToggleQuestionOpened('next')} >
           <span className="question-details-button-next">Next</span>
         </button>
       </div>
